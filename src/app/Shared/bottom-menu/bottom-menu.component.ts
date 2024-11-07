041fefa32fca5cc2e19be17aebe2b988d60cc2d1
baovo <baovhtn@imt-soft.com>
2024-11-07 17:29:21 +0700

    create menu bottom update login Page update menu logo

---
+++ src/app/Shared/bottom-menu/bottom-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-bottom-menu',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './bottom-menu.component.html',
+  styleUrl: './bottom-menu.component.scss',
+})
+export class BottomMenuComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MenuComponent } from './Shared/menu/menu.component';
+import { BottomMenuComponent } from './Shared/bottom-menu/bottom-menu.component';
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterModule, MenuComponent],
+  imports: [RouterModule, MenuComponent, BottomMenuComponent],
 })
 export class AppComponent {
   title = 'CookingRecipes';
